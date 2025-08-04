de06deb40daae1e40d5b437e2d26c0abb26c8fa2
Marco Wehnert <marco.wehnert@gmx.de>
2025-08-04 10:05:21 +0200

    Added plugin telescope and treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
+    config = function()
+	local configs = require("nvim-treesitter.configs")
+	configs.setup({
+	    highlight = {
+		enable = true,
+	    },
+	    indent = { enable = true },
+	    autotage = { enable = true },
+	    ensure_installed = {
+		"dockerfile",
+		"lua",
+		"markdown",
+		"python",
+		"yaml",
+	    },
+	    auto_install = false,
+	})
+    end
+}
