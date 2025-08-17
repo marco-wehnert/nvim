e8ef49de24ed8b0f4be0dd6dff7feba30bbcc040
Marco Wehnert <marco.wehnert@gmx.de>
2025-08-18 00:41:28 +0200

    Added lsp-zero, seems that autocomplete is working

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -18,6 +18,29 @@ return {
             {'rafamadriz/friendly-snippets'},
         },
         config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-n>'] = cmp.mapping.select_next_item(),
+                    ['<C-p>'] = cmp.mapping.select_prev_item(),
+                    ['<CR>'] = cmp.mapping.confirm({select=true}),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                }),
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'buffer' },
+                },
+            })
+
             local lsp = require("lsp-zero").preset({})
             lsp.on_attach(function(client, bufnr)
                 print("LSP attached:", client.name)
