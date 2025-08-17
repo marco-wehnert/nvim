edb7e5c3cad8319626a65cd17511aa0a784484db
Marco Wehnert <marco.wehnert@gmx.de>
2025-08-17 22:43:26 +0200

    Got first auto complete working

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,16 +1,21 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
+--	{
+--		"hrsh7th/cmp-nvim-lsp",
+--	},
 	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
+--			"rafamadriz/friendly-snippets",
 		},
 	},
 	{
 		"hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lsp",
+        },
 		config = function()
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
@@ -35,9 +40,8 @@ return {
 				sources = cmp.config.sources({
 					-- { name = 'nvim_lsp' },
 					-- { name = 'vsnip' }, -- For vsnip users.
-					{ name = "luasnip" }, -- For luasnip users.
-				}, {
 					{ name = "buffer" },
+					{ name = "luasnip" }, -- For luasnip users.
 				}),
 			})
 		end,
