5d04f21278bf89107fa850b523eb8a095e43b283
Marco Wehnert <marco.wehnert@gmx.de>
2025-08-21 16:20:29 +0200

    Close nvim when neo-tree is the last window

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,5 +5,18 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
-    }
+    },
+    config = function()
+        require("neo-tree").setup({})
+        vim.api.nvim_create_autocmd("BufEnter", {
+            callback = function()
+                if vim.bo.filetype == "neo-tree" then
+                    local wins = vim.api.nvim_list_wins()
+                    if #wins == 1 then
+                        vim.cmd("quit")
+                    end
+                end
+            end,
+        })
+    end,
 }
