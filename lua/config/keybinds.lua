3eab6b1a6c13f755b454a44f163df6b4e59b9d4d
Marco Wehnert <marco.wehnert@gmx.de>
2025-11-16 18:14:16 +0100

    Rebase on remote branch

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,18 +1,27 @@
 -- To show all keybinds:
 -- :Telescope keymaps
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+
+-- Next commented command opens a view to choose a file which shall be
+-- opened, but it makes problem when the current file hasn't been
+-- saved yet.
+-- vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+
 -- Navigate between panes
 vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+
 -- Leave insert mode in terminal with <Esc>
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 -- Show neo-tree
 vim.keymap.set("n", "<leader>nt", ":Neotree show<CR>", { noremap = true, silent = true })
+
 -- Toggle copy mode
+-- Copy mode displays only the content, and only the content, nothing more
 vim.keymap.set("n", "<leader>cm", ":CopyMode<CR>", { noremap = true, silent = true })
+
 -- Open lazygit
 vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>", { noremap = true, silent = true })
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,25 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    branch = "master",
+    lazy = false,
     build = ":TSUpdate",
-    config = function()
-	local configs = require("nvim-treesitter.configs")
-	configs.setup({
-	    highlight = {
-		enable = true,
-	    },
-	    indent = { enable = true },
-	    autotage = { enable = true },
-	    ensure_installed = {
-		"dockerfile",
-		-- "latex",
-		"lua",
-		"markdown",
-		"python",
-		"yaml",
-	    },
-	    auto_install = true,
-	})
-    end
+--    config = function()
+--	local configs = require("nvim-treesitter.configs")
+--	configs.setup({
+--	    highlight = {
+--		enable = true,
+--	    },
+--	    indent = { enable = true },
+--	    autotage = { enable = true },
+--	    ensure_installed = {
+--		"dockerfile",
+--		-- "latex",
+--		"lua",
+--		"markdown",
+--		"python",
+--		"yaml",
+--	    },
+--	    auto_install = true,
+--	})
+--    end
 }
