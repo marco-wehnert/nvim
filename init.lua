0383c1ffa579d958afc6b193d23c266a208bc746
Marco Wehnert <marco.wehnert@gmx.de>
2025-11-17 02:44:26 +0100

    Add LSP for Python (add pyright in init.lua)

--- init.lua
+++ init.lua
@@ -19,11 +19,24 @@ require("config.options")
 require("config.keymap")
 require("config.lazy")
 
+
+-- LSP for Lua --------------------------------------------------------
 vim.lsp.config("lua_ls", {
-    cmd = { "lua-language-server" },
-    filetypes = { "lua" },
-    root_markers = { ".luarc.json", ".luarc.jsonc" },
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = { ".luarc.json", ".luarc.jsonc" },
 })
 
 vim.lsp.enable('lua_ls')
---vim.lsp.enable('pyright')
+
+-- LSP for Python -----------------------------------------------------
+-- local cmp_nvim_lsp = require("cmp_nvim_lsp")
+-- local capabilities = cmp_nvim_lsp.default_capabilities()
+vim.lsp.config("pyright", {
+  cmd = { "pyright-langserver", "--stdio" },
+  filetypes = { "python" },
+  root_markers = { "pyproject.toml", "setup.py", ".git" },
+--  capabilities = capabilities,
+})
+
+vim.lsp.enable('pyright')
